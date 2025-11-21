664f214b4b4893b01123c957325213c42da74129
dainisstrumpe <dainisstrumpe@startschool.org>
2025-11-21 15:06:37 +0200

    nov21_1

---
+++ scripts/learn.js
@@ -0,0 +1,21 @@
+// setTimeout(() => {
+//   console.log('hello after 5 seconds');
+// }, 5000);
+
+
+// let interval = setInterval(() => {
+//   console.log('one second passed');
+// }, 1000);
+
+// setTimeout(() => {
+//   clearInterval(interval);
+//   console.log('stopped');
+// }, 5000);
+
+const array = ['viens', 'divi', 'trīs', 'četri', 'pieci'];
+
+array.forEach((item, i) => {
+  setTimeout(() => {
+    console.log(item);
+  }, 2000 * i);
+});
